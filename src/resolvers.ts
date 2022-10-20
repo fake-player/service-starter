3824c8b6ab14999f0242c5e3f49f0e4ec63b5481
Temirlan <your.darling.itf@proton.me>
2022-10-20 19:28:39 +0500

    Type check resolvers

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,7 +1,8 @@
 import { DateTimeResolver } from 'graphql-scalars'
 import { example } from './resolvers/query/example'
+import { Resolvers } from './__generated__/graphql'
 
-export const resolvers = {
+export const resolvers: Resolvers = {
   DateTime: DateTimeResolver,
   Query: {
     example,
