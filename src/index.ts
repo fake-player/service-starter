6848d13b187ffc30dd44e583692422508dfadec3
Temirlan <your.darling.itf@proton.me>
2022-10-23 11:35:20 +0500

    Add config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { startStandaloneServer } from '@apollo/server/standalone'
 import { createServer } from './server'
+import { port } from './config/config.json'
 
 async function startApolloServer() {
   const server = await createServer()
 
   const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
+    listen: { port },
   })
 
   console.log(`🚀  Server ready at ${url}`)
