d61c608e6f7c2b2cab3458195c3ce880e3be72bf
Temirlan <your.darling.itf@proton.me>
2022-10-23 11:48:49 +0500

    Move to relative import cause of error

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,4 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  moduleNameMapper: {
-    '^src/(.*)': '<rootDir>/src/$1',
-  },
 }
